b823f318fdb82b1aff7f9ac0fbffe2460875d5f7
Igor Prusevich <iprusevich@gmail.com>
2019-02-10 01:25:54 +0300

    Update FancyCalc.cs

--- FancyCalc/FancyCalc.cs
+++ FancyCalc/FancyCalc.cs
@@ -31,7 +31,64 @@ namespace FancyCalc
         //generic calc method. usage: "10 + 20"  => result 30
         public double Culculate(string expression)
         {
-            throw new NotImplementedException();
+            double[] a = new double[expression.Length];
+            double[] b = new double[expression.Length];
+            char[] ax = new char[expression.Length];
+            ax = expression.ToCharArray();
+            string oper = "";
+            int operaind = 5;
+            for (int i = 0; i < expression.Length; i++)
+
+            {
+                if (char.IsDigit(ax[i]))
+                {
+                    a[i] = (int)Char.GetNumericValue(ax[i]);
+
+                }
+                else
+                {
+                    oper = ax[i].ToString();
+                    operaind = i;
+                    break;
+                }
+            }
+            for (int i = operaind + 1; i < expression.Length; i++)
+
+            {
+                if (char.IsDigit(ax[i]))
+                {
+                    b[i] = (int)Char.GetNumericValue(ax[i]);
+
+                }
+                else
+                {
+                    break;
+                }
+            }
+
+            Array.Resize(ref a, operaind);
+            string a1 = string.Join("", a);
+            string b1 = string.Join("", b);
+            double first = Convert.ToDouble(a1);
+            double second = Convert.ToDouble(b1);
+            double finish = 0;
+            switch (oper)
+            {
+                case "+":
+                    finish = first + second;
+                    break;
+
+                case "-":
+                    finish = first - second;
+                    break;
+                case "*":
+                    finish = first * second;
+                    break;
+                default:
+                    break;
+            }
+            return finish;
+
 
         }
     }
